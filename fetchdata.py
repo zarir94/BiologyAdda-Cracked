deb124080862eec0e534df692b1c7c330ff53bca
zarir94 <shahriarzariracc@gmail.com>
2025-09-03 12:12:38 +0600

    first commit

---
+++ fetchdata.py
@@ -0,0 +1,61 @@
+from py_mini_racer import MiniRacer
+from requests import get
+from bs4 import BeautifulSoup
+import re, json, time
+
+
+def modGet(*a, **kw):
+    try:
+        r = get(*a, **kw)
+        if r.status_code == 500:
+            time.sleep(5)
+            return modGet(*a, **kw)
+        return r
+    except Exception as e:
+        print('modGet:', e)
+        return modGet(*a, **kw)
+
+
+def get_single_course_data(slug: str):
+    data = modGet('https://api.biologyadda.com/api/courses/' + slug).json()
+    course = dict(title=data['title'], slug=data['slug'], img=data['image']['link'], sections=[])
+
+    for s in data['sections']:
+        asec = dict(title=s['title'], slug=s['slug'], contents=[])
+        for c in s['contents']:
+            acon = dict(title=c['title'], slug=c['slug'], type=c['type'])
+            rd = modGet('https://api.biologyadda.com/api/content/' + c["slug"]).json()['data'][c['type']]
+            if c['type'] == 'video':
+                acon = acon | dict(source=rd['source'], link=rd['link'])
+            elif c['type'] == 'link' or c['type'] == 'pdf':
+                acon['link'] = rd['link']
+            elif c['type'] == 'note':
+                acon['html'] = rd['body']
+            elif c['type'] == 'exam':
+                pass
+            else:
+                raise Exception('Unsupported content type %s. Data: %s' % (c['type'], str(rd)))
+            asec['contents'].append(acon)
+        course['sections'].append(asec)
+
+    return course
+
+
+def get_all_course_data():
+    all_courses = []
+    done_slugs = []
+    rd = modGet('https://api.biologyadda.com/api/course-category').json()['data']
+    for s, t in {l['slug']: l['title'] for l in rd}.items():
+        rd = modGet('https://api.biologyadda.com/api/courses?category_slug=' + s).json()['data']
+        for c in rd:
+            if c['slug'] in done_slugs: continue
+            all_courses.append(get_single_course_data(c['slug']) | dict(category=dict(title=t, slug=s)))
+            done_slugs.append(c['slug'])
+    return all_courses
+
+
+
+if __name__ == '__main__':
+    cd = get_all_course_data()
+    # print(cd)
+    json.dump(cd, open('data.json', 'w'), indent=4)
