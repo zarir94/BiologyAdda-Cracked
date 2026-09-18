ad619e119371d1e9381028ff24ff7fd118042466
S.M.Shahriar Zarir <148766170+zarir94@users.noreply.github.com>
2026-09-18 12:50:34 +0600

    Disable host redirection in hooks.server.ts
    Comment out host redirection logic in handle function.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,12 @@
 import { redirect, type Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	let host = event.request.headers.get('Host');
+	// let host = event.request.headers.get('Host');
 	// console.log(host);
 	
-	if (host == 'bacracked.onrender.com') {
-		redirect(301, new URL(event.url.pathname, 'http://bacracked.top'))
-	}
+	// if (host == 'bacracked.onrender.com') {
+		// redirect(301, new URL(event.url.pathname, 'http://bacracked.top'))
+	// }
 
 	const response = await resolve(event);
 	return response;
